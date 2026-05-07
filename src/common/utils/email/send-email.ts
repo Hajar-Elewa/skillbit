ca243fdbad20200aed1e6d5eb83b3f712b15fc43
Hajar-Elewa <___GitHub>
2026-05-07 23:26:30 +0300

    update

--- src/common/utils/email/send-email.ts
+++ src/common/utils/email/send-email.ts
@@ -1,30 +1,57 @@
 import * as nodemailer from 'nodemailer';
 import 'dotenv/config';
-import * as dns from 'dns'; 
+import { Resend } from 'resend';
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, 
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },  
-  tls: {
-  rejectUnauthorized: false
-},
-  connectionTimeout: 10000, // مهم
- 
-})
-
-
-export const sendEmail = async ( 
-  options: nodemailer.SendMailOptions,
-)=> {
-  await transporter.sendMail(options) 
-  return true
+interface SendEmailOptions {
+  to: string;
+  subject: string;
+  html: string;
 }
+
+export const sendEmail = async ({
+  to,
+  subject,
+  html,
+}: SendEmailOptions) => {
+  try {
+    await resend.emails.send({
+      from: `${process.env.EMAIL}`,
+      to,
+      subject,
+      html,
+    });
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp.gmail.com',
+//   port: 587,
+//   secure: false, 
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASS,
+//   },  
+//   tls: {
+//   rejectUnauthorized: false
+// },
+//   connectionTimeout: 10000, // مهم
+ 
+// })
+
+
+// export const sendEmail = async ( 
+//   options: nodemailer.SendMailOptions,
+// )=> {
+//   await transporter.sendMail(options) 
+//   return true
+// }
 
 
 //every time we call sendEmail function it will create a new transporter which is not efficient so we can create transporter once and reuse it every time we call sendEmail function.ودا في الطريقة اللي فوق
