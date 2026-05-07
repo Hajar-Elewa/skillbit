10673f8e2cb9c2248a99c624e92406a0eebcd71f
Hajar-Elewa <___GitHub>
2026-05-07 23:39:47 +0300

    update

--- src/Modules/Auth/auth.service.ts
+++ src/Modules/Auth/auth.service.ts
@@ -253,18 +253,18 @@ export class AuthService {
 
     const otp = generateOTP(6)
 
-    // const emailSent = await sendEmail({
-    //   to: email,
-    //   from: process.env.EMAIL,
-    //   subject: 'Reset Password',
-    //   html: `<h1>Hello ${user.fullname}</h1> 
-    //              <p>Your reset password OTP is: <strong>${otp}</strong></p>
-    //              <p>This OTP will expire in 10 minutes.</p>`
-    // })
+    const emailSent = await sendEmail({
+      to: email,
+      from: process.env.EMAIL,
+      subject: 'Reset Password',
+      html: `<h1>Hello ${user.fullname}</h1> 
+                 <p>Your reset password OTP is: <strong>${otp}</strong></p>
+                 <p>This OTP will expire in 10 minutes.</p>`
+    })
 
-    // if (!emailSent) {
-    //   throw new InternalServerErrorException('Failed to send email, please try again')
-    // }
+    if (!emailSent) {
+      throw new InternalServerErrorException('Failed to send email, please try again')
+    }
 
     //update user with new otp and set it expires in 10 minutes
     await this.userRepo.Update({
--- src/common/utils/email/send-email.ts
+++ src/common/utils/email/send-email.ts
@@ -1,57 +1,27 @@
 import * as nodemailer from 'nodemailer';
 import 'dotenv/config';
-import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
-interface SendEmailOptions {
-  to: string;
-  subject: string;
-  html: string;
-}
-
-export const sendEmail = async ({
-  to,
-  subject,
-  html,
-}: SendEmailOptions) => {
-  try {
-    await resend.emails.send({
-      from: `${process.env.EMAIL}`,
-      to,
-      subject,
-      html,
-    });
-
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,        // 👈 change from 465 to 587
+  secure: false,    // 👈 change from true to false
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+  tls: {
+    rejectUnauthorized: false
   }
-};
-
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 587,
-//   secure: false, 
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASS,
-//   },  
-//   tls: {
-//   rejectUnauthorized: false
-// },
-//   connectionTimeout: 10000, // مهم
- 
-// })
+})
 
 
-// export const sendEmail = async ( 
-//   options: nodemailer.SendMailOptions,
-// )=> {
-//   await transporter.sendMail(options) 
-//   return true
-// }
+export const sendEmail = async ( 
+  options: nodemailer.SendMailOptions,
+)=> {
+  await transporter.sendMail(options) 
+  return true
+}
 
 
 //every time we call sendEmail function it will create a new transporter which is not efficient so we can create transporter once and reuse it every time we call sendEmail function.ودا في الطريقة اللي فوق
